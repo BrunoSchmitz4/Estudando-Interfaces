fff75902aa0b6b7efb46be3ba669afc6e0d55365
Bruno Silva <brunosch250@gmail.com>
2023-07-06 10:02:28 -0300

    Initial commit

---
+++ Entities/Invoice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace InterAppOne.Entities
+{
+    internal class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+
+        // Retornar para double se necessário
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return $"Basic payment: R$ {BasicPayment.ToString("F2", CultureInfo.InvariantCulture)}\n" +
+                $"Tax: {Tax.ToString("F2", CultureInfo.InvariantCulture)}%\n" +
+                $"Total payment: R$ " + TotalPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using InterAppOne.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Transactions;
+
+namespace InterAppOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicles();
+        }
+
+        static void Vehicles()
+        {
+            Console.WriteLine("Dados do Veículo: ");
+            Console.Write("Modelo do carro: ");
+            string model = Console.ReadLine();
+            Console.Write("Data e Hora de Entrada (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Data e Hora de Saída (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+        
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+        }
+        
+        static void InitGeometricsF()
+        {
+            
+
+            try
+            {
+                string option = "".ToUpper();
+                option.ToUpper();
+                while ((option != "C") || (option != "Q") 
+                    || (option != "R") || (option != "T"))
+                {
+                    Console.WriteLine("Option foi salvo com um: " + option);
+                    Console.WriteLine("Selecione uma forma geométrica das abaixo:");
+                    Console.Write("(C) - Círculo | (Q) - Quadrado | " +
+                        "(R) - Retângulo | (T) - Triângulo: ");
+                    option = Console.ReadLine().ToUpper();
+                    option.ToUpper();
+                    if (option == "C")
+                    {
+                        Console.WriteLine("Escolheu círculo");
+                    }
+                    else if (option == "R")
+                    {
+                        Console.WriteLine("Escolheu retângulo");
+                    }
+                    else if (option == "T")
+                    {
+                        Console.WriteLine("Escolheu triângulo");
+
+                    }
+                    else if (option == "Q")
+                    {
+                        Console.WriteLine("Escolheu quadrado");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
